c0c0e993d797b39c6c6550b23dbe4183e8ebbfa4
ElenaZenkova <lenysik_omsk@mail.ru>
2019-01-22 00:46:13 +0600

    stickers_NEW_2

--- stickers.js
+++ stickers.js
@@ -8,56 +8,30 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 
         await driver.get('http://localhost/litecart/en/');
 
-// 1 block
-            let bmp = await driver.findElements(By.xpath('//*[@id="box-most-popular"]/div/ul/li[contains(@class, "hover")]'))
-            console.log("Товаров box-most-popular: " + bmp.length)
-            console.log("-----------")
+        console.log("--------------------------")
+        let items = await driver.findElements(By.className('product column shadow hover-light'))
+        console.log("Товаров всего: " + items.length)
+        console.log("--------------------------")
 
 
-            for (var i = 1; i < bmp.length + 1; i++) {
-                let item1 = await driver.findElement(By.xpath('//*[@id="box-most-popular"]/div/ul/li[contains(@class, "hover")][' + i + ']')).getText()
-                console.log(i + ": " + item1.valueOf())
+        let sticks = await driver.findElements(By.className('sticker'))
+        console.log("Всего стикеров: " + sticks.length)
+        console.log("--------------------------")
+
+        for (var i = 0; i < sticks.length + 1; i++) {
 
 
-                let stick1 = await driver.findElements(By.xpath('//*[@id="box-most-popular"]/div/ul/li['+i+']/a[1]/div[1]/div'))
-                console.log("количество стиков: " + stick1.length)
-                console.log("-------------")
+        let sticker = await driver.findElements(By.xpath('(//*["box"]/div/ul/li //div[@title])[' + i + ']'))
+
+        if (sticker = 1) {
+
+            console.log("Стикер у товара: " + sticker.valueOf());
+        }
+        else if (sticker > 1)
+            console.log("Стикеров у товара: " + sticker.valueOf());
+        else
+            console.log("Стикеров нет у товара: " + sticker.valueOf());
             }
-
-
-// 2 block
-            let bc = await driver.findElements(By.xpath('//*[@id="box-campaigns"]/div/ul/li[contains(@class, "hover")]'))
-            console.log("Товаров box-campaigns: " + bc.length)
-            console.log("-----------")
-
-
-            for (var j = 1; j < bc.length + 1; j++) {
-                let item2 = await driver.findElement(By.xpath('//*[@id="box-campaigns"]/div/ul/li[contains(@class, "hover")][' + j + ']')).getText()
-                console.log(j + ": " + item2.valueOf())
-
-
-                let stick2 = await driver.findElements(By.xpath('//*[@id="box-campaigns"]/div/ul/li['+j+']/a[1]/div[1]/div'))
-                console.log("количество стиков: " + stick2.length)
-                console.log("-------------")
-            }
-
-
-// 3 block0
-            let blp = await driver.findElements(By.xpath('//*[@id="box-latest-products"]/div/ul/li[contains(@class, "hover")]'))
-            console.log("Товаров box-latest-products: " + blp.length)
-            console.log("-----------")
-
-
-            for (var k = 1; k < blp.length + 1; k++) {
-                let item3 = await driver.findElement(By.xpath('//*[@id="box-latest-products"]/div/ul/li[contains(@class, "hover")][' + k + ']')).getText()
-                console.log(k + ": " + item3.valueOf())
-
-                let stick3 = await driver.findElements(By.xpath('//*[@id="box-latest-products"]/div/ul/li['+k+']/a[1]/div[1]/div'))
-                console.log("количество стиков: " + stick3.length)
-                console.log("-------------")
-
-            }
-
 
     }
     finally {
