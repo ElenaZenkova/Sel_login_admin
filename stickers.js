c1d378ff9501e8fced6970572094ba732b95ee13
ElenaZenkova <lenysik_omsk@mail.ru>
2019-01-22 22:15:52 +0600

    stickers_new_3

--- stickers.js
+++ stickers.js
@@ -1,18 +1,15 @@
 
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
-
 (async function example() {
     let driver = await new Builder().forBrowser('chrome').build();
     try {
-
         await driver.get('http://localhost/litecart/en/');
 
         console.log("--------------------------")
-        let items = await driver.findElements(By.className('product column shadow hover-light'))
+        let items = await driver.findElements(By.className('image-wrapper'))
         console.log("Товаров всего: " + items.length)
         console.log("--------------------------")
-
 
         let sticks = await driver.findElements(By.className('sticker'))
         console.log("Всего стикеров: " + sticks.length)
@@ -20,11 +17,9 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 
         for (var i = 0; i < sticks.length + 1; i++) {
 
-
-        let sticker = await driver.findElements(By.xpath('(//*["box"]/div/ul/li //div[@title])[' + i + ']'))
+        let sticker = await driver.findElements(By.className('(sticker)[' + i + ']'))
 
         if (sticker = 1) {
-
             console.log("Стикер у товара: " + sticker.valueOf());
         }
         else if (sticker > 1)
@@ -32,7 +27,6 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
         else
             console.log("Стикеров нет у товара: " + sticker.valueOf());
             }
-
     }
     finally {
         await driver.quit();
