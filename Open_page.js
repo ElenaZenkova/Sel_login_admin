ab15d7275bb6b21efa5e8616311b23d69196a1a2
ElenaZenkova <lenysik_omsk@mail.ru>
2019-02-06 01:08:59 +0600

    Open_page_2

--- Open_page.js
+++ Open_page.js
@@ -6,45 +6,37 @@ const {Builder, By,  until} = require('selenium-webdriver');
         await driver.get('http://localhost/litecart/en/');
         await driver.wait(until.titleIs('Online Store | My Store', ));
 
-        // а) на главной странице и на странице товара совпадает текст названия товара
-        // б) на главной странице и на странице товара совпадают цены (обычная и акционная)
-        // в) обычная цена зачёркнутая и серая (можно считать, что "серый" цвет это такой, у которого в RGBa представлении одинаковые значения для каналов R, G и B)
-        // г) акционная жирная и красная (можно считать, что "красный" цвет это такой, у которого в RGBa представлении каналы G и B имеют нулевые значения)
-        // (цвета надо проверить на каждой странице независимо, при этом цвета на разных страницах могут не совпадать)
-        // д) акционная цена крупнее, чем обычная (это тоже надо проверить на каждой странице независимо)
-
 
 // part 1
 
-        let name1 = await driver.findElement(By.css('#box-campaigns > div > ul > li > a.link > div.name')).getAttribute('textContent');
-        let price_reg1 = await driver.findElement(By.css('#box-campaigns > div > ul > li > a.link > div.price-wrapper > s')).getAttribute('textContent');
-        let price_reg1_line = await driver.findElement(By.css('#box-campaigns > div > ul > li > a.link > div.price-wrapper > s')).getCssValue('text-decoration-line');
-        let price_reg1_color = await driver.findElement(By.css('#box-campaigns > div > ul > li > a.link > div.price-wrapper > s')).getCssValue('color');
-        let price_reg1_size = await driver.findElement(By.css('#box-campaigns > div > ul > li > a.link > div.price-wrapper > s')).getCssValue('font-size');
+        let name1 = await driver.findElement(By.css('#box-campaigns div.name')).getAttribute('textContent');
+        let price_reg1 = await driver.findElement(By.css('#box-campaigns s')).getAttribute('textContent');
+        let price_reg1_line = await driver.findElement(By.css('#box-campaigns s')).getCssValue('text-decoration-line');
+        let price_reg1_color = await driver.findElement(By.css('#box-campaigns s')).getCssValue('color');
+        let price_reg1_size = await driver.findElement(By.css('#box-campaigns s')).getCssValue('font-size');
 
-        let price_akc1 = await driver.findElement(By.css('#box-campaigns > div > ul > li > a.link > div.price-wrapper > strong')).getAttribute('textContent');
-        let price_akc1_fat = await driver.findElement(By.css('#box-campaigns > div > ul > li > a.link > div.price-wrapper > strong')).getCssValue('font-weight');
-        let price_akc1_color = await driver.findElement(By.css('#box-campaigns > div > ul > li > a.link > div.price-wrapper > strong')).getCssValue('color');
-        let price_akc1_size = await driver.findElement(By.css('#box-campaigns > div > ul > li > a.link > div.price-wrapper > strong')).getCssValue('font-size');
+        let price_akc1 = await driver.findElement(By.css('#box-campaigns strong')).getAttribute('textContent');
+        let price_akc1_fat = await driver.findElement(By.css('#box-campaigns strong')).getCssValue('font-weight');
+        let price_akc1_color = await driver.findElement(By.css('#box-campaigns strong')).getCssValue('color');
+        let price_akc1_size = await driver.findElement(By.css('#box-campaigns strong')).getCssValue('font-size');
 
 
-
-        await driver.findElement(By.css('#box-campaigns > div > ul > li > a.link > div.image-wrapper > img')).click();
+        await driver.findElement(By.css('#box-campaigns .link')).click();
         await driver.wait(until.titleIs('Yellow Duck | Subcategory | Rubber Ducks | My Store'));
 
 // part 2
 
-        let name2 = await driver.findElement(By.css('#box-product > div:nth-child(1) > h1')).getAttribute('textContent');
-        let price_reg2 = await driver.findElement(By.css('#box-product > div.content > div.information > div.price-wrapper > s')).getAttribute('textContent');
-        let price_reg2_line = await driver.findElement(By.css('#box-product > div.content > div.information > div.price-wrapper > s')).getCssValue('text-decoration-line');
-        let price_reg2_color = await driver.findElement(By.css('#box-product > div.content > div.information > div.price-wrapper > s')).getCssValue('color');
-        let price_reg2_size = await driver.findElement(By.css('#box-product > div.content > div.information > div.price-wrapper > s')).getCssValue('font-size');
+        let name2 = await driver.findElement(By.css('h1')).getAttribute('textContent');
+        let price_reg2 = await driver.findElement(By.css('#box-product s')).getAttribute('textContent');
+        let price_reg2_line = await driver.findElement(By.css('#box-product s')).getCssValue('text-decoration-line');
+        let price_reg2_color = await driver.findElement(By.css('#box-product s')).getCssValue('color');
+        let price_reg2_size = await driver.findElement(By.css('#box-product s')).getCssValue('font-size');
 
 
-        let price_akc2 = await driver.findElement(By.css('#box-product > div.content > div.information > div.price-wrapper > strong')).getAttribute('textContent');
-        let price_akc2_fat = await driver.findElement(By.css('#box-product > div.content > div.information > div.price-wrapper > strong')).getCssValue('font-weight');
-        let price_akc2_color = await driver.findElement(By.css('#box-product > div.content > div.information > div.price-wrapper > strong')).getCssValue('color');
-        let price_akc2_size = await driver.findElement(By.css('#box-product > div.content > div.information > div.price-wrapper > strong')).getCssValue('font-size');
+        let price_akc2 = await driver.findElement(By.css('.campaign-price')).getAttribute('textContent');
+        let price_akc2_fat = await driver.findElement(By.css('.campaign-price')).getCssValue('font-weight');
+        let price_akc2_color = await driver.findElement(By.css('.campaign-price')).getCssValue('color');
+        let price_akc2_size = await driver.findElement(By.css('.campaign-price')).getCssValue('font-size');
 
         // сравнение
 
@@ -78,6 +70,7 @@ const {Builder, By,  until} = require('selenium-webdriver');
             console.log('Цена акционная товара разная: ' + price_akc1.valueOf() + '; ' + price_akc2.valueOf());
         }
         //
+
         console.log('----------------------------------------------------------------------------------------------------------');
         console.log('Обычная цена-1 зачёркнутая и серая: ' + price_reg1_line.valueOf() + '; ' + price_reg1_color.valueOf());
 
@@ -109,8 +102,6 @@ const {Builder, By,  until} = require('selenium-webdriver');
             console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Акционная цена-2 меньше, чем обычная: ' + price_akc2_size.valueOf() + ' < ' + price_reg2_size.valueOf());
         }
-
-
 
     }
     finally {
