ee99ff9aca0c2d7e811520205b4e1baac759eacf
ElenaZenkova <lenysik_omsk@mail.ru>
2019-02-07 21:51:19 +0600

    Open_Page_new

--- Open_page.js
+++ Open_page.js
@@ -42,64 +42,93 @@ const {Builder, By,  until} = require('selenium-webdriver');
 
         if (name1 == name2)
         {
-            console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Текст названия товара одинаковый: ' + name1.valueOf() + '; ' + name2.valueOf());
         }
         else {
-            console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Текст названия товара разный: ' + name1.valueOf() + '; ' + name2.valueOf());
         }
 
         if (price_reg1 == price_reg2)
         {
-            console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Цена обычная товара одинаковая: ' + price_reg1.valueOf() + '; ' + price_reg2.valueOf());
         }
         else {
-            console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Цена обычная товара разная: ' + price_reg1.valueOf() + '; ' + price_reg2.valueOf());
         }
 
         if (price_akc1 == price_akc2)
         {
-            console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Цена акционная товара одинаковая: ' + price_akc1.valueOf() + '; ' + price_akc2.valueOf());
         }
         else {
-            console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Цена акционная товара разная: ' + price_akc1.valueOf() + '; ' + price_akc2.valueOf());
         }
-        //
 
-        console.log('----------------------------------------------------------------------------------------------------------');
-        console.log('Обычная цена-1 зачёркнутая и серая: ' + price_reg1_line.valueOf() + '; ' + price_reg1_color.valueOf());
+        //обычная цена зачёркнутая и серая (можно считать, что "серый" цвет это такой,
+        // у которого в RGBa представлении одинаковые значения для каналов R, G и B)
 
-        console.log('----------------------------------------------------------------------------------------------------------');
-        console.log('Обычная цена-2 зачёркнутая и серая: ' + price_reg2_line.valueOf() + '; ' + price_reg2_color.valueOf());
+        let value1 = price_reg1_color.valueOf().toString();
+        value1 = value1.substring(5,value1.length-1);
+        let colors1 = value1.split(', ');
+        console.log(colors1);
+        if(colors1[0] === colors1[1] && colors1[1] === colors1[2]){
+            console.log('Обычная цена-1 зачёркнутая и серая: ' + price_reg1_line.valueOf() + '; ' + price_reg1_color.valueOf());
+        }
+        else {
+            console.log('Обычная цена-1 зачёркнутая, но не серая: ' + price_reg1_line.valueOf() + '; ' + price_reg1_color.valueOf());
+        }
 
-        console.log('----------------------------------------------------------------------------------------------------------');
-        console.log('Акционная цена-1 жирная и красная: ' + price_akc1_fat.valueOf() + '; ' + price_akc1_color.valueOf());
+        let value2 = price_reg2_color.valueOf().toString();
+        value2 = value2.substring(5,value2.length-1);
+        let colors2 = value2.split(', ');
+        console.log(colors2);
+        if(colors2[0] === colors2[1] && colors2[1] === colors2[2]) {
+            console.log('Обычная цена-2 зачёркнутая и серая: ' + price_reg2_line.valueOf() + '; ' + price_reg2_color.valueOf());
+        }
+        else{
+            console.log('Обычная цена-2 зачёркнутая, но не серая: ' + price_reg2_line.valueOf() + '; ' + price_reg2_color.valueOf());
+        }
 
-        console.log('----------------------------------------------------------------------------------------------------------');
-        console.log('Акционная цена-2 жирная и красная: '  + price_akc2_fat.valueOf() + '; ' + price_akc2_color.valueOf());
+        //акционная жирная и красная (можно считать, что "красный" цвет это такой,
+        // у которого в RGBa представлении каналы G и B имеют нулевые значения)
+
+        let value3 = price_akc1_color.valueOf().toString();
+        value3 = value3.substring(5,value3.length-1);
+        let colors3 = value3.split(', ');
+        console.log(colors3);
+        if(colors3[1] === colors3[2]) {
+            console.log('Акционная цена-1 жирная и красная: ' + price_akc1_fat.valueOf() + '; ' + price_akc1_color.valueOf());
+        }
+        else{
+            console.log('Акционная цена-1 жирная, но не красная: ' + price_akc1_fat.valueOf() + '; ' + price_akc1_color.valueOf());
+        }
+
+        let value4 = price_akc1_color.valueOf().toString();
+        value4 = value4.substring(5,value4.length-1);
+        let colors4 = value4.split(', ');
+        console.log(colors4);
+        if(colors4[1] === colors4[2]) {
+            console.log('Акционная цена-2 жирная и красная: ' + price_akc2_fat.valueOf() + '; ' + price_akc2_color.valueOf());
+        }
+        else{
+            console.log('Акционная цена-2 жирная, но не красная: ' + price_akc2_fat.valueOf() + '; ' + price_akc2_color.valueOf());
+        }
+
 
         if (price_akc1_size > price_reg1_size)
         {
-            console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Акционная цена-1 крупнее, чем обычная: ' + price_akc1_size.valueOf() + ' > ' + price_reg1_size.valueOf());
         }
         else {
-            console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Акционная цена-1 меньше, чем обычная: ' + price_akc1_size.valueOf() + ' < ' + price_reg1_size.valueOf());
         }
 
+
         if (price_akc2_size > price_reg2_size)
         {
-            console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Акционная цена-2 крупнее, чем обычная: ' + price_akc2_size.valueOf() + ' > ' + price_reg2_size.valueOf());
         }
         else {
-            console.log('----------------------------------------------------------------------------------------------------------');
             console.log('Акционная цена-2 меньше, чем обычная: ' + price_akc2_size.valueOf() + ' < ' + price_reg2_size.valueOf());
         }
 
